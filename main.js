e2fd94501d05b4c79ea0bcf357895d35eaa47b23
andychen00 <andychen9979@gmail.com>
2023-02-27 17:19:56 +0700

    fix skill

--- main.js
+++ main.js
@@ -58,6 +58,23 @@ tabLink.forEach((tab, index) => {
     });
 });
 
+//skill accordian 
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    });
+}
+
+
 //Swiper navigation project
 let swiper = new Swiper(".mySwiper", {
     navigation: {
